5f7ae30d1882ebaba756cbbdab7d1e58f3572399
michael dimmitt <michaelgdimmitt@gmail.com>
2019-03-14 15:52:03 -0400

    move side effect IO.inspect to the mix task.

--- lib/matcher.ex
+++ lib/matcher.ex
@@ -1,14 +1,7 @@
 defmodule Matcher do
   @moduledoc """
   Documentation for Matcher.
-  """
 
-  def handle_match( input ) do
-    matcher(input)
-    |> IO.inspect()
-  end
-
-  @doc """
   https://hexdocs.pm/elixir/Kernel.html
   # is_atom(term) is_binary(term) is_bitstring(term) is_boolean(term)
   # is_float(term) is_function(term) is_function(term, arity)
@@ -19,18 +12,18 @@ defmodule Matcher do
   # Guards are a way to augment pattern matching with more complex checks.
   matcher function implements a bunch of different gaurds.
   """
-  def matcher(number) when is_integer(number) and rem(number, 2) == 0, do: 'number was not dividable by 2'
+  def handle_match(number) when is_integer(number) and rem(number, 2) == 0, do: 'number was not dividable by 2'
 
-  def matcher(term) when is_integer(term), do: { term, 'is integer'}
-  def matcher(term) when is_float(term), do: { term, 'converted to integer:', round(term) }
-  def matcher(term) when is_integer(term) or is_float(term), do: 'matches on integer float is also true'
-  def matcher(val) when map_size(val) == 0 when tuple_size(val) == 0, do: 'an empty map or tuple was found!'
+  def handle_match(term) when is_integer(term), do: { term, 'is integer'}
+  def handle_match(term) when is_float(term), do: { term, 'converted to integer:', round(term) }
+  def handle_match(term) when is_integer(term) or is_float(term), do: 'matches on integer float is also true'
+  def handle_match(val) when map_size(val) == 0 when tuple_size(val) == 0, do: 'an empty map or tuple was found!'
 
   @doc """
     the tuples and lists that have content have not been matched allowing the below functions to execute.
   """
-  def matcher(cool_list = [a, b, c] = [head | tail ] = [1,2,3]), do: { 'cool_list:', cool_list, 'a:', a, 'b:', b, 'c:', c, 'head:', head, 'tail:', tail}
-  def matcher(cool_tuple = { s, [a, b, c] = [head | tail ] = [1,2,3] }), do: { s, a, b, c, head, tail, cool_tuple}
+  def handle_match(cool_list = [a, b, c] = [head | tail ] = [1,2,3]), do: { 'cool_list:', cool_list, 'a:', a, 'b:', b, 'c:', c, 'head:', head, 'tail:', tail}
+  def handle_match(cool_tuple = { s, [a, b, c] = [head | tail ] = [1,2,3] }), do: { s, a, b, c, head, tail, cool_tuple}
   # matcher({x, y, [1, 2, 3]})
 
 end
--- lib/mix/tasks/matcher.ex
+++ lib/mix/tasks/matcher.ex
@@ -2,13 +2,18 @@ defmodule Mix.Tasks.Matcher do
   use Mix.Task
 
   @shortdoc "runs the handle match  function many different ways."
+  def matcher( input ) do
+    Matcher.handle_match(input)
+    |> IO.inspect()
+  end
+
   def run(_) do
-    Matcher.handle_match(5.2)
-    Matcher.handle_match(5)
-    Matcher.handle_match({})
-    Matcher.handle_match(%{})
-    Matcher.handle_match([1 | [ 2, 3] ])
-    Matcher.handle_match({ 3, [1 | [ 2, 3] ]})
+    matcher(5.2)
+    matcher(5)
+    matcher({})
+    matcher(%{})
+    matcher([1 | [ 2, 3] ])
+    matcher({ 3, [1 | [ 2, 3] ]})
   end
 
 end
