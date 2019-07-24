4ef6337dda84e5c4e185f3383ac45adfcd221262
MichaelDimmitt <michaelgdimmitt@gmail.com>
2019-07-23 18:01:24 -0700

    Update CheckTree.ex

--- lib/CheckTree.ex
+++ lib/CheckTree.ex
@@ -1,12 +1,12 @@
 defmodule CheckTree do
   # https://hexdocs.pm/elixir/master/guards.html#defining-custom-guard-expressions
   # the suggested way to write a custom guard.
-  defguard is_not_tree(number) when 'tree' != number
+  defguard is_not_tree(strValue) when 'tree' != strValue
 
   # not recommended way of writing a custom gaurd.
-  defmacro is_tree(number) do
+  defmacro is_tree(strValue) do
     quote do
-      'tree' == unquote(number)
+      'tree' == unquote(strValue)
     end
   end
 end
